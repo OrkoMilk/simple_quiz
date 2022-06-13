05534f8fb20c419e8d4bcfc3263c3b3bb467851d
Orest <orko.slim@gmail.com>
2022-06-13 21:26:34 +0300

    change application routes added quiz route

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'flavor_banner.dart';
 import 'screens/home_screen.dart';
+import 'screens/quiz_screen.dart';
 import 'screens/splash_screen.dart';
 
 const String homeScreenRoute = '/home_screen';
 const String splashScreenRoute = '/splash_screen';
+const String quizScreenRoute = '/quiz_screen';
 
 Map<String, WidgetBuilder> applicationRoutes = <String, WidgetBuilder>{
   splashScreenRoute: (context) => SplashScreen(),
   homeScreenRoute: (context) => const FlavorBanner(child: HomeScreen()),
+  quizScreenRoute: (context) => const QuizScreen(),
 };
